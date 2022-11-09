400cfe23d9e5810297dee073868181a8c1773552
jamesvuong25 <jvuong@zagmail.gonzaga.edu>
2022-11-09 00:02:24 -0800

    Got reload working

--- docs/script.js
+++ docs/script.js
@@ -345,29 +345,46 @@ window.onload = function () {
     };
 
     //function to reset starting game info
-    reloadButton = document.getElementById(reloadButton);
     // reloadButtons.addEventListener("click", function() {
     //     console.log("HI");
     // });
     function resetBoard(resetLives) {
         //reset paddle position
-        paddleHeight = 10;
-        paddleWidth = 75;
-        paddleX = (canvas.width - paddleWidth) / 2;
-
-        //reset bricks
+        var ballRadius = 10;
+        var x = canvas.width / 2;
+        var y = canvas.height - 30;
+        var dx = 2;
+        var dy = -2;
+        var paddleHeight = 10;
+        var paddleWidth = 75;
+        var paddleX = (canvas.width - paddleWidth) / 2;
+        var rightPressed = false;
+        var leftPressed = false;
+        var brickRowCount = 5;
+        var brickColumnCount = 3;
+        var brickWidth = 75;
+        var brickHeight = 20;
+        var brickPadding = 10;
+        var brickOffsetTop = 30;
+        var brickOffsetLeft = 30;
+        var score = 0;
+        var lives = 3;
+        var pause = true;
+    
+        var bricks = [];
+    
         for (var c = 0; c < brickColumnCount; c++) {
             bricks[c] = [];
             for (var r = 0; r < brickRowCount; r++) {
                 bricks[c][r] = { x: 0, y: 0, status: 1 };
             }
-        }     
-
-        //reset score and lives
-        score = 0;
-        lives = 3;
-
+        }  
     };
+
+    reloadButton = document.getElementById("reloadButton");
+    reloadButton.addEventListener("click", function() {
+        window.location.reload();
+    })
 
     //draw the menu.
     //we don't want to immediately draw... only when we click start game 
